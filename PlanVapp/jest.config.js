44ba0491952edd35866bf52fe2d3c7e974aeba5b
Grim <122496893+TeamGrimoire@users.noreply.github.com>
2024-12-03 13:45:57 -0500

    Update jest.config.js

--- PlanVapp/jest.config.js
+++ PlanVapp/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: {
+    '@testing-library/jest-native/extend-expect'
+    ],
+    transofrmIgnorePatterns: [
+      'node_modules/(?!(jest-)?react-native|@react-native|react-navigation|@react-navigation)'
+      ]
 };
