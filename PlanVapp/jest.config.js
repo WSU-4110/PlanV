8b44956b827148094275b991c6f2238cd784169b
Grim <122496893+TeamGrimoire@users.noreply.github.com>
2024-12-03 18:00:05 -0500

    Update jest.config.js

--- PlanVapp/jest.config.js
+++ PlanVapp/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'react-native',
-    "testTimeout": 10000
+    "testTimeout": 10000,
     transformIgnorePatterns: [
       'node_modules/(?!(jest-)?react-native|@react-native|react-navigation|@react-navigation)'
       ]
