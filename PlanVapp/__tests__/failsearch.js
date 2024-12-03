6375e99cd7942ec5abe2f3277a55445a0d88b723
Grim <122496893+TeamGrimoire@users.noreply.github.com>
2024-12-03 17:06:19 -0500

    weather.js unit tests

---
+++ PlanVapp/__tests__/failsearch.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import Weather from '../../screens/Weather';
+
+global.fetch = jest.fn();
+describe('Connectivity Test', () => {
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
+  test('Sends error upon network error.', async () => {
+    fetch.mockImplementationOnce(() => 
+      Promise.reject(new Error('Network error'))
+    );
+
+    const { getByPlaceholderText, getByText, findByText } = render(<Weather />);
+    
+    fireEvent.changeText(getByPlaceholderText('Enter city'), 'Berlin');
+    fireEvent.press(getByText('Search'));
+
+    const errorMessage = await findByText('Contact us for this error. Could not fetch weather.');
+    expect(errorMessage).toBeTruthy();
+  });
+});
