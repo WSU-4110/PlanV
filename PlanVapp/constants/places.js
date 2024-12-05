5a4abc743d30c9ce8a37b9200ec1d0f62e1e9241
Grim <122496893+TeamGrimoire@users.noreply.github.com>
2024-12-05 14:15:39 -0500

    Update places.js

--- PlanVapp/constants/places.js
+++ PlanVapp/constants/places.js
@@ -5,6 +5,10 @@ const places = [
     location: 'UAE',
     image: require('../assets/Dubai.png'),
     details: `One second.`,
+    coordinates: {
+      latitude: 25.276987,
+      longitude: 55.296249,
+    },
   },
   {
     id: '2',
@@ -12,13 +16,21 @@ const places = [
     location: 'Japan',
     image: require('../assets/Japan.png'),
     details: `One second.`,
+    coordinates: {
+      latitude: 35.6762,
+      longitude: 139.6503,
+    },
   },
   {
     id: '3',
     name: 'Paris',
     location: 'France',
     image: require('../assets/Paris.png'),
-    details: `Hi.One second.`,
+    details: `Hi. One second.`,
+    coordinates: {
+      latitude: 48.8566,
+      longitude: 2.3522,
+    },
   },
   {
     id: '4',
@@ -26,6 +38,10 @@ const places = [
     location: 'Italy',
     image: require('../assets/Italy.png'),
     details: `Hi. One second.`,
+    coordinates: {
+      latitude: 43.7167,
+      longitude: 10.4000,
+    },
   },
 ];
 
