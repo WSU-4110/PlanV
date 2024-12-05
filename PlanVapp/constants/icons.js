649664dcd62441b62d9f38c8023c41daae3a89ad
Hi <hm3480@wayne.edu>
2024-12-05 16:29:22 -0500

    Update icons.js

--- PlanVapp/constants/icons.js
+++ PlanVapp/constants/icons.js
@@ -6,6 +6,14 @@ export const bookmark = require('./../assets/icons/bookmark_icon.png');
 export const home = require('./../assets/icons/home_icon.png');
 export const search = require('./../assets/icons/search_icon.png');
 export const user = require('./../assets/icons/user_icon.png');
+export const map = require('./../assets/icons/map_icon.png');
+export const cloud = require('./../assets/icons/cloud_icon.png');
+export const MG = require('./../assets/icons/magnifying_icon.png');
+export const star = require('./../assets/icons/star.png');
+export const LP = require('./../assets/icons/location_pin.png');
+export const person = require('./../assets/icons/person_icon.png');
+export const notification = require('./../assets/icons/notification.png');
+export const house = require('./../assets/icons/house_icon.png');
 
 export default {
     airplane,
@@ -16,4 +24,12 @@ export default {
     home,
     search,
     user,
+    map,
+    cloud,
+    MG,
+    star,
+    LP,
+    person,
+    notification,
+    house,
 };
