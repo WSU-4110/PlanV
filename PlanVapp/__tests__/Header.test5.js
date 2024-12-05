daedb81078d175a21589d2312f6eafe929b28ed3
Hi <hm3480@wayne.edu>
2024-12-05 17:53:57 -0500

    Update Header.test5.js

--- PlanVapp/__tests__/Header.test5.js
+++ PlanVapp/__tests__/Header.test5.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react-native';  // Import 'screen'
-import { NavigationContainer } from '@react-navigation/native'; // Wrapping in NavigationContainer
-import Header from '../components/Header';
+import { render } from '@testing-library/react-native';
+import FlightFilters from '../Screens/Booking/FlightFilters';
 
-test('initially, the Hotel button has a border and others do not', () => {
-  render(
-    <NavigationContainer>
-      <Header />
-    </NavigationContainer>
-  );
+describe('FlightFilters Component', () => {
+  test('displays error message when no flight data is available', () => {
+    const route = { params: { flightData: [] } };
+    const { getByText } = render(<FlightFilters route={route} />);
 
-  // Check that the "Hotel" button has a border initially
-  const hotelButton = screen.getByTestId('hotelButton');
-  expect(hotelButton.props.style.borderColor).toBe('white');
-
-  // Check that the "Flight" button does not have a border initially
-  const flightButton = screen.getByTestId('flightButton');
-  expect(flightButton.props.style.borderColor).toBe('transparent');
-
-  // Check that the "Car" button does not have a border initially
-  const carButton = screen.getByTestId('carButton');
-  expect(carButton.props.style.borderColor).toBe('transparent');
+    expect(getByText('No flight data available.')).toBeTruthy();
+  });
 });
